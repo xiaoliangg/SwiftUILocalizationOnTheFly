3fb88ce1917f9f0719f97146fbd081da8c5c01ef
xiaoliangg <xiaoliangyuu@163.com>
2022-05-01 22:07:36 +0800

    first commit

---
+++ SwiftUILocalizationOnTheFly/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUILocalizationOnTheFly
+//
+//  Created by yl on 2022/5/1.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUILocalizationOnTheFly/SwiftUILocalizationOnTheFlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUILocalizationOnTheFlyApp.swift
+//  SwiftUILocalizationOnTheFly
+//
+//  Created by yl on 2022/5/1.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUILocalizationOnTheFlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
